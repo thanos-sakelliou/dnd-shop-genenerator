49606f931369e547c2e6bbe9a9cba85c4ee079b6
thanos-sakelliou <thanos.sakelliou@gmail.com>
2021-04-04 02:23:56 +0300

    Ui cleanup,  fixed(almost) auto-reseting

--- 5eprices.py
+++ 5eprices.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-
+import time
 
 ###############################
 SPELLCASTERS = ["Warlock", "Cleric", "Wizard", "Sorcerer", "Ranger"]
@@ -32,7 +32,7 @@ def better_price(row):
 
 def price_fluctuation(row):
     random_variance = np.random.randint(low=-row['Suggested Price']/20-1, high=row['Suggested Price']/20)*10
-    return  '{0:+d}'.format(random_variance) 
+    return  random_variance 
 
 def multiple_occurances(items, number):
     items = items.sample(n=number, replace=True)
@@ -66,9 +66,8 @@ def populate_with_spells(items, spellcasters=[], name="Scroll of "):
     items['Item'] = items.apply(add_spell, axis=1)
     return items
 
-######MAIN#################
-
-def split_and_sample_items(relevant_items, equipment_items_number, mage_items_number, consumable_items_number, health_potion_items_number, scroll_items_number):
+@st.cache(hash_funcs={pd.DataFrame: lambda _: None, int: lambda _: None, str: lambda _: None, list: lambda _: None })
+def sample_consumable_items(relevant_items, consumable_items_number, health_potion_items_number, scroll_items_number):
     #split to categories
     consumable_items = relevant_items[relevant_items['Type'].isin(CONSUMABLE)]
     consumable_items = consumable_items[consumable_items['Rarity'].isin(selected_rarities)]
@@ -76,13 +75,6 @@ def split_and_sample_items(relevant_items, equipment_items_number, mage_items_nu
     scroll_items = consumable_items[consumable_items['Type'] == 'Spell Scrolls']
     consumable_items = consumable_items[~consumable_items['Item'].str.contains('Potion Of Healing', na=False)]
     consumable_items = consumable_items[~(consumable_items['Type'] == 'Spell Scrolls')]
-
-    equipment_items = relevant_items[relevant_items['Type'].isin(EQUIPMENT)]
-    equipment_items = equipment_items[equipment_items['Rarity'].isin(selected_rarities)]
-
-    mage_items = relevant_items[relevant_items['Type'].isin(MAGE)]
-    mage_items = mage_items[mage_items['Rarity'].isin(selected_rarities)]
-    populate_with_spells(mage_items, [], "Wand Of ")
 
     #sample number of items
     sampled_consumable_items = consumable_items.sample(n=consumable_items_number, replace=True)
@@ -98,12 +90,28 @@ def split_and_sample_items(relevant_items, equipment_items_number, mage_items_nu
     sampled_consumable_items = sampled_consumable_items.append(sampled_health_potion_items)
     sampled_consumable_items = sampled_consumable_items.append(sampled_scroll_items)
 
+    return sampled_consumable_items
+
+@st.cache(hash_funcs={pd.DataFrame: lambda _: None, int: lambda _: None, str: lambda _: None, list: lambda _: None })
+def sample_equipment_items(relevant_items, equipment_items_number):
+    equipment_items = relevant_items[relevant_items['Type'].isin(EQUIPMENT)]
+    equipment_items = equipment_items[equipment_items['Rarity'].isin(selected_rarities)]
+
     sampled_equipment_items = equipment_items.sample(n=equipment_items_number)
+
+    return sampled_equipment_items
+
+@st.cache(hash_funcs={pd.DataFrame: lambda _: None, int: lambda _: None, str: lambda _: None, list: lambda _: None })
+def sample_mage_items(relevant_items, mage_items_number):
+    mage_items = relevant_items[relevant_items['Type'].isin(MAGE)]
+    mage_items = mage_items[mage_items['Rarity'].isin(selected_rarities)]
+    populate_with_spells(mage_items, [], "Wand Of ")
 
     sampled_mage_items = mage_items.sample(n=mage_items_number)
 
-    return sampled_consumable_items, sampled_equipment_items, sampled_mage_items
+    return sampled_mage_items
 
+@st.cache(hash_funcs={pd.DataFrame: lambda _: None, int: lambda _: None, str: lambda _: None, list: lambda _: None })
 def load_items(df):
     #filter with prices
     relevant_items = df[df['DMPG Price'].notnull()]
@@ -121,12 +129,18 @@ def export_to_excel(sampled_consumable_items, sampled_equipment_items, sampled_m
         sampled_equipment_items.to_excel(writer, sheet_name='Equipment')
         sampled_mage_items.to_excel(writer, sheet_name='Mage items')
 
-def style_options():
+def streamlit_style_options():
     st.markdown( 
     """<style>
         .stDataFrame div {text-align: left !important}
     </style>
     """, unsafe_allow_html=True) 
+
+def dataframe_style(df):
+    return df.reset_index(drop=True).style.hide_index().format({"Fluctuation": "{0:+d}",
+                                                                "Page": "{0:.0f}",
+                                                                "DMPG Price": "{0:.0f}"})
+    
 
 #TODO
 #ratio use
@@ -139,28 +153,35 @@ def style_options():
 
 #------------------------MAIN-----------------------------------------
 
-st.title('5e Magic Shop Items Generator')
+st.set_page_config(page_title='5e Magic Shop Generator', 
+                    page_icon="🛒", 
+                    layout='wide', 
+                    initial_sidebar_state='expanded')
 
-col1, col2 = st.beta_columns([1,3])
-cont1 = st.beta_container()
-cont2 = st.beta_container()
-cont3 = st.beta_container()
-style_options()
+st.title('5e Magic Shop Generator')
+streamlit_style_options()
 
-with st.beta_container():
-    basic_rarity_id = st.selectbox("Items basic rarity", options=[0,1,2,3,4,5], index=1, format_func=lambda o:RARITIES[o], help=None)
+st.sidebar.header("Generation options")
 
-with col1:
-    st.write("------------")
-    spellcasters = st.multiselect("Spellcaster classes for scrolls", SPELLCASTERS, default=None)
-    selected_rarities = [RARITIES[i] for i in range(basic_rarity_id+1)]
-    consumable_items_number = st.slider("Consumables number", min_value=0, max_value=20, value=5, step=1)
-    health_potion_items_number = st.slider("Health potions number", min_value=0, max_value=20, value=5, step=1)
-    scroll_items_number = st.slider("Scrolls number", min_value=0, max_value=20, value=5, step=1)
-    st.write("------------")
-    equipment_items_number = st.slider("Equipment & wondrous items number", min_value=0, max_value=20, value=8, step=1)
-    st.write("------------")
-    mage_items_number = st.slider("Spellcaster items number", min_value=0, max_value=20, value=5, step=1)
+st.sidebar.subheader("General")
+basic_rarity_id = st.sidebar.selectbox("Items max rarity", options=[0,1,2,3,4,5], index=1, format_func=lambda o:RARITIES[o], help=None)
+st.sidebar.write("------------")
+
+st.sidebar.subheader("Consumables")
+spellcasters = st.sidebar.multiselect("Spellcaster classes for scrolls", SPELLCASTERS, default=None)
+selected_rarities = [RARITIES[i] for i in range(basic_rarity_id+1)]
+scroll_items_number = st.sidebar.slider("Scrolls number", min_value=0, max_value=20, value=5, step=1)
+consumable_items_number = st.sidebar.slider("Consumables number", min_value=0, max_value=20, value=5, step=1)
+health_potion_items_number = st.sidebar.slider("Health potions number", min_value=0, max_value=20, value=5, step=1)
+st.sidebar.write("------------")
+
+st.sidebar.subheader("Equipment & Wondrous Items")
+equipment_items_number = st.sidebar.slider("Equipment & wondrous items number", min_value=0, max_value=20, value=8, step=1)
+st.sidebar.write("------------")
+
+st.sidebar.subheader("Spellcaster items")
+mage_items_number = st.sidebar.slider("Spellcaster items number", min_value=0, max_value=20, value=5, step=1)
+
 
 
 
@@ -168,17 +189,23 @@ df = pd.read_excel(("D&D Magical Item Prices.xlsx"))
 df.drop(['DMG Price'], axis=1)
 # print(list(df['Rarity'].unique()))
 relevant_items = load_items(df)
-sampled_consumable_items, sampled_equipment_items, sampled_mage_items = split_and_sample_items(relevant_items, equipment_items_number, mage_items_number, consumable_items_number, health_potion_items_number, scroll_items_number)
 
 
-with col2:
-    st.header("Consumables")
-    st.dataframe(sampled_consumable_items, height=500)
+sampled_consumable_items = sample_consumable_items(relevant_items, consumable_items_number, health_potion_items_number, scroll_items_number)
+sampled_equipment_items = sample_equipment_items(relevant_items, equipment_items_number)
+sampled_mage_items = sample_mage_items(relevant_items, mage_items_number)
 
-    st.header("Equipment and Wondrous Items")
-    st.dataframe(sampled_equipment_items)
+reset = st.button("Generate Shop")
+if reset:
+    st.caching.clear_cache()
 
-    st.header("Spellcaster Items")
-    st.dataframe(sampled_mage_items)
+st.header("Consumables")
+st.dataframe(dataframe_style(sampled_consumable_items), height=500)
 
-    
+st.header("Equipment and Wondrous Items")
+st.dataframe(dataframe_style(sampled_equipment_items))
+
+st.header("Spellcaster Items")
+st.dataframe(dataframe_style(sampled_mage_items))
+
+
