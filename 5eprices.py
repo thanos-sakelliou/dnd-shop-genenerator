95f7fdb6d0602ed8e6a87feb867bab11e307a9a1
thanos-sakelliou <thanos.sakelliou@gmail.com>
2021-04-11 17:57:12 +0300

    description update

--- 5eprices.py
+++ 5eprices.py
@@ -158,10 +158,13 @@ st.set_page_config(page_title='5e Magic Shop Generator',
                     layout='wide', 
                     initial_sidebar_state='expanded')
 
-st.title('5e Magic Shop Generator')
 streamlit_style_options()
 
 st.sidebar.header("Generation options")
+
+reset = st.sidebar.button("Restock Shop")
+if reset:
+    st.caching.clear_cache()
 
 st.sidebar.subheader("General")
 basic_rarity_id = st.sidebar.selectbox("Items max rarity", options=[0,1,2,3,4,5], index=1, format_func=lambda o:RARITIES[o], help=None)
@@ -195,9 +198,17 @@ sampled_consumable_items = sample_consumable_items(relevant_items, consumable_it
 sampled_equipment_items = sample_equipment_items(relevant_items, equipment_items_number)
 sampled_mage_items = sample_mage_items(relevant_items, mage_items_number)
 
-reset = st.button("Generate Shop")
-if reset:
-    st.caching.clear_cache()
+st.title('5e Magic Shop Generator')
+
+st.markdown('''
+### A magic shop generator for D&D 5e, with currated prices.
+
+##### This is a tool to help Dungeon Masters populate the item stock of a magic shop. Items have a suggested price which is derived from a combination of 2 currated price lists, as well as a random fluncuation of the price, simulating the current trending of the market. It also provides customization of the rarity and the number of items generated, to create any magic shop, from a wandering potion merchant to a magic-megastore in Eberron.
+
+###### *This tool is made to ease any DM who wants to incorporate magic shops in their campaign, without putting much effort. One should know, that magic shops are not an official mechanic. It is a homebrew rule used by many DMs (who doesn't want to find an artifact on a dusty shelf?), that sometimes can be too complicated, or game breaking if it is not done correctly. 5th edition's official rules say that magic items are provided mostly by loot, and the power balance of the characters is build on top of this idea. An optional rule is proposed in Xanathar's guide to everything, to be able to find a rare seller of few items. Thus, by using this shop, one must take care not to overload characters with magic items. Items should be expensive (in relation to their gold) and a few in quantity.*
+###### *This shop generator is build around the idea that very few magic shops/merchants exist, that have a small stock of expensive magic items, useful to the players or not. Their stock changes every few weeks or sessions to give players the chance to find something useful for their character. This serves the concept of a real market, to give players the idea that optimizing their character is done in a natural way; without the DM forcing items on them.*
+
+''')
 
 st.header("Consumables")
 st.dataframe(dataframe_style(sampled_consumable_items), height=500)
